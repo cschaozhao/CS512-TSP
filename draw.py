632cb4a65585b2b663fe84b050c1d7536ac66b02
cschaozhao <80139314+cschaozhao@users.noreply.github.com>
2021-05-23 01:39:05 +0800

    Update draw.py

--- draw.py
+++ draw.py
@@ -3,7 +3,42 @@ import matplotlib
 from matplotlib import pyplot as plt
 import math
 
-route = [1, 2, 0, 3, 4]
+
+def compute_coordinates(Distance):
+    d = Distance[0][1]
+    X = [0, 0]
+    Y = [0, d]
+    a = Distance[0][2]
+    b = Distance[1][2]
+    y2 = ((a ** 2 - b ** 2) / d + d) / 2
+    x2 = (a ** 2 - y2 ** 2) ** 0.5
+    X.append(x2)
+    Y.append(y2)
+    for i in range(len(Distance) - 3):
+        index = i + 3
+        a = Distance[0][index]
+        b = Distance[1][index]
+        y = ((a ** 2 - b ** 2) / d + d) / 2
+        x = (a ** 2 - y ** 2) ** 0.5
+        if (x - x2) ** 2 + (y - y2) ** 2 != Distance[2][index] ** 2:
+            x = 0 - x
+        X.append(x)
+        Y.append(y)
+    return X, Y
+
+
+def draw_real(route, Distance):
+    X, Y = compute_coordinates(Distance)
+    for i in range(len(X)):
+        plt.plot(X[i], Y[i], 'ob')
+    for index in range(len(route)):
+        index2 = index + 1
+        if index == len(route) - 1:
+            index2 = 0
+        plt.plot([X[route[index]], X[route[index2]]],
+                 [Y[route[index]], Y[route[index2]]], 'r')
+    plt.show()
+
 
 
 def calculate_position(ox, oy, radius, num):
